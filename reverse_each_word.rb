4a624a2b8eb80200204fbfb72abd0e6e7f1c38fe
Gerald Tee <93geraldtee@gmail.com>
2019-09-24 21:04:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
+#def reverse_each_word(sentence1)
+#  sentence1.split.collect {|reverse| reverse.reverse}.join(" ")
+#end
+
 def reverse_each_word(sentence1)
-  sentence1.split.collect {|reverse| reverse.reverse}.join(" ")
+  sentence1.split.collect do 
+    |reverse| reverse.reverse
+    end
+    sentence1.join(" ")
 end
