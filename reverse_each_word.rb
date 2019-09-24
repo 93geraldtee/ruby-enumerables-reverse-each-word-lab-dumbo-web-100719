c8b4207de9d52be0547b12339c0541244ceccf94
Gerald Tee <93geraldtee@gmail.com>
2019-09-24 21:09:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-#def reverse_each_word(sentence1)
-#  sentence1.split.collect {|reverse| reverse.reverse}.join(" ")
-#end
-
 def reverse_each_word(sentence1)
-  sentence1.split.collect do 
-    |reverse| reverse.reverse
-    end
-    reverse.join(" ")
+  sentence1.split.collect {|reverse| reverse.reverse}.join(" ")
 end
+
+#def reverse_each_word(sentence1)
+#  sentence1.split.collect do 
+#    |reverse| reverse.reverse
+#    end
+#    reverse.join(" ")
+#end
